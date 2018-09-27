5bebeadf141c45af7e974f44c7e7fd4582e01a53
ChrisWOle <chriswole@gmail.com>
2018-09-27 13:33:53 +0200

    VS project added

---
+++ ATM_App/ATM/Seperation.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Seperation
+    {
+        public void calculateDistances()
+        {
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void raiseAlarm(Track tag1, Track tag2)
+        {
+
+        }
+
+        public void deactivateAlarm(Track tag1, Track tag2)
+        {
+
+        }
+    }
+}
