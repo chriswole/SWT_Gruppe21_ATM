6576e0fde81134fabbd1fa9e374106ea5eb25bc1
Mette <mettechrisjacobsen@hotmail.com>
2018-09-27 13:40:02 +0200

    Track and position added

---
+++ ATM_App/ATM/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace ATM
+{
+    public class Position
+    {
+        public Position(int x, int y)
+        {
+            int x_ = x;
+            int y_ = y;
+        }
+
+        private int x_;
+        private int y_;
+    }
+}
--- ATM_App/ATM/Track.cs
+++ ATM_App/ATM/Track.cs
@@ -8,5 +8,19 @@ namespace ATM
 {
     class Track
     {
+        public Track(string tag, Position pos, int altitude, int horivelocity, int course)
+        {
+            string tag_ = tag;
+            Position pos_ = pos;
+            int altitude_ = altitude;
+            int hori_velocity_ = horivelocity;
+            int course_ = course;
+        }
+
+        private string tag_;
+        Position pos_;
+        private int altitude_;
+        private int hori_velocity_;
+        private int course_;
     }
 }
