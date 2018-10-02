b62ad5e4d6bbb15949ef5c05ba72984a9d655915
ChrisWOle <chriswole@gmail.com>
2018-10-02 09:59:13 +0200

    Implemented calculateDistance

--- ATM_App/ATM/Seperation.cs
+++ ATM_App/ATM/Seperation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,22 +10,40 @@ namespace ATM
 {
     class Seperation
     {
+        public List<Danger> newDangers_;
+        public List<Danger> OldDangers_;
+
+        public Seperation()
+        {
+            newDangers_ = new List<Danger>();
+            OldDangers_ = new List<Danger>();
+        }
+
         public void calculateDistances(ToTrack toTrackObj)
         {
-           
             foreach (var track1 in toTrackObj.tracks_)
             {
                 foreach (var track2 in toTrackObj.tracks_)
                 {
                     Position dist = track1.pos_ - track2.pos_;
-                    dist = dist.x_
+                    var distance = Math.Sqrt(dist.x_ ^ 2 + dist.y_ ^ 2);
+                    if (distance<5000)
+                    {
+                        int alt = Math.Abs(track1.altitude_ - track2.altitude_);
+                        if (alt<300)
+                        {
+                            Danger dangerObj = new Danger(track1, track2, (int) distance);
+                            newDangers_.Add(dangerObj);
+                            
+                        }
+                    }
                 }
                 
             }
             
         }
 
-
+       
 
 
 
