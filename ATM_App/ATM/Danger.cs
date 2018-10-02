29bbd5992e2eed0d53a5a8de5da3d43052d4fe6f
ChrisWOle <chriswole@gmail.com>
2018-10-02 09:58:16 +0200

    Added Danger class

---
+++ ATM_App/ATM/Danger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Danger
+    {
+        public Danger(Track track1, Track track2, int dist)
+        {
+            track1_ = track1;
+            track2_ = track2;
+            distance_ = dist; 
+        }
+        public Track track1_ { get; set; }
+        public Track track2_ { get; set; }
+        public int distance_ { get; set; }
+
+    }
+}
