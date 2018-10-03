620ce1e3fe1b0b1fb11924a0a422797ecba6eeb5
Mette <mettechrisjacobsen@hotmail.com>
2018-10-03 11:11:33 +0200

    added timestamp

--- ATM_App/ATM/Track.cs
+++ ATM_App/ATM/Track.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ATM
 {
-    public class Track:ITrack
+    public class Track : ITrack
     {
-        public Track(string tag = "", Position pos = null, int altitude = 0, int horiVelocity = 0, int course = 0)
+        public Track(string tag = "", Position pos = null, int altitude = 0, int horiVelocity = 0, int course = 0,
+            int timestamp = 0)
         {
             string tag_ = tag;
             Position pos_ = pos;
             int altitude_ = altitude;
             int hori_velocity_ = horiVelocity;
             int course_ = course;
+            int timestamp_ = timestamp;
         }
 
         public string tag_ { get; set; }
@@ -22,6 +25,7 @@ namespace ATM
         public int altitude_ { get; set; }
         public int hori_velocity_ { get; set; }
         public int course_ { get; set; }
+        public int timestamp_{get; set;}
 
-    }
+}
 }
