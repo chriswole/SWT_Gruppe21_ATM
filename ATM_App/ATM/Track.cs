a80112405ec5f3872e2584bac6ea60d9aac710d9
Mette <mettechrisjacobsen@hotmail.com>
2018-10-03 12:18:04 +0200

    Monitor started

--- ATM_App/ATM/Track.cs
+++ ATM_App/ATM/Track.cs
@@ -26,7 +26,7 @@ namespace ATM
         public int altitude_ { get; set; }
         public int hori_velocity_ { get; set; }
         public int course_ { get; set; }
-        public int timestamp_{get; set;}
+        public int timestamp_ { get; set; }
 
-}
+    }
 }
