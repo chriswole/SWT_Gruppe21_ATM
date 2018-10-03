6774514a5b93cdd138c2109f3b465cb5da0918c8
ChrisWOle <chriswole@gmail.com>
2018-10-03 11:08:57 +0200

    Added ILogger and Logger + implementation

---
+++ ATM_App/ATM/Interfaces/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Interfaces
+{
+    interface ILogger
+    {
+        void AddToLog(IDanger danger2Log);
+    }
+}
