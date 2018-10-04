12b74e4fd3ba0116d2e07f32b3b3fc4530dfaddf
ChrisWOle <chriswole@gmail.com>
2018-10-04 13:19:48 +0200

    Danger Test Done

--- ATM_App/ATMUnitTest/ATMDangerUnitTest.cs
+++ ATM_App/ATMUnitTest/ATMDangerUnitTest.cs
@@ -21,6 +21,8 @@ namespace ATMUnitTest
         
         Track track1;
         Track track2;
+        int dist;
+
 
         [SetUp]
         public void Setup()
@@ -29,17 +31,17 @@ namespace ATMUnitTest
             Position pos2= new Position(54210, 48214);
             track1 = new Track("TAG4S", pos1, 10000, 587, 85,"20180215");
             track2 = new Track("KRS8S", pos2, 14005, 421, 254, "20181005");
-            
-            _uut = new Danger(track1_, track2_,2500 );
+            dist = 2500;
+            _uut = new Danger(track1, track2,dist );
         }
 
         
         [Test]
-        public void ConstructorAndGetTest()
+        public void DangerConstructorAndGetTest()
         { 
             Assert.That(_uut.track1_, Is.EqualTo(track1));
             Assert.That(_uut.track2_, Is.EqualTo(track2));
-            Assert.That(_uut.distance_, Is.EqualTo(2500));
+            Assert.That(_uut.distance_, Is.EqualTo(dist));
         }
     }
 }
