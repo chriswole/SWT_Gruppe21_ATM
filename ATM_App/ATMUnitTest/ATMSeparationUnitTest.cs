50f7366ea832f93930b33cac6a5486510297ae26
ChrisWOle <chriswole@gmail.com>
2018-10-04 16:08:15 +0200

    Preparing for test, and made notes for caldist test,

--- ATM_App/ATMUnitTest/ATMSeparationUnitTest.cs
+++ ATM_App/ATMUnitTest/ATMSeparationUnitTest.cs
@@ -18,17 +18,34 @@ namespace ATMUnitTest
     {
        
 
-        ISeparation TestSeparation = new Separation();
+        ISeparation TestSeparation;
+        List<Track> trackList;
+        Position pos1, pos2, pos3, pos4;
+        Track T1, T2, T3, T4;
 
 
         [SetUp]
-
-
-
         public void Setup()
         {
-          
+            TestSeparation = new Separation();
+            trackList = new List<Track>();
+            pos1 = new Position(27541,25884);
+            pos2 = new Position(22513, 75141);
+            pos3 = new Position(30245, 28884);
+            T1 = new Track("H5JS", pos1,5422,251,150,"20181005");
+            //T2 = new Track();
+            // Skal test ingen danger 
+            // alt too close, dist out of range
+            // alt too close dist too close , not same tag
+
         }
+
+        [Test]
+        public void CalculateDistanceTest(List<Track> trackList)
+        {
+
+        }
+
         /*
         [Test]
         public void raiseAlarmIf()
